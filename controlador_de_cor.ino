2f04ab1afdf5a77dff52e49003b8bbb1ed41728d
Lucas Soares <lsoares267@gmail.com>
2017-07-26 13:58:45 -0300

    Upando código

---
+++ controlador_de_cor.ino
@@ -0,0 +1,26 @@
+const int potA = 2;
+const int potB = 3;
+const int potC = 4;
+
+const int ledR = 3;
+const int ledG = 5;
+const int ledB = 6;
+
+void setup(){
+  pinMode(ledR,OUTPUT);
+  pinMode(ledG,OUTPUT);
+  pinMode(ledB,OUTPUT);
+}
+
+void loop(){
+  potsValue(potA,ledR);
+  potsValue(potB,ledG);
+  potsValue(potC,ledB);
+}
+
+void potsValue(int potPin, int ledPin){
+  int valPot = analogRead(potPin);
+  valPot = map(valPot,0,1023,0,255);
+  analogWrite(ledPin,valPot);
+}
+
